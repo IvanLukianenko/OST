0f67de1ca2bda619c1fec5d9f977fd44d9a3473d
LukianenkoIvan <lukianenko.ia@phystech.edu>
2022-05-20 18:16:34 +0300

    some code changes

--- code/model/model.py
+++ code/model/model.py
@@ -14,7 +14,7 @@ class GRU(nn.Module):
     def forward(self, x):
         h0 = torch.randn(self.num_layers, x.shape[0],  self.hidden_dim)
         out, (hn) = self.gru(x, h0)
-        out = self.fc(out[-1])
+        out = self.fc(out[:, -1, :])
         out = torch.sigmoid(out)
         return out
 
@@ -36,3 +36,32 @@ def train(model, train_dl, num_epochs, opt, loss_fn):
         losses.append(loss.detach().item())
         print(f'Epoch: {epoch} | Loss: {loss.detach().item()}')
     return losses
+
+
+def test(model,
+         test_dl: torch.utils.data.DataLoader):
+    r"""
+
+    Args:
+        model:
+        test_dl:
+        metrics:
+        metrics_funcs:
+
+    Returns:
+        predictions: list of predictions for testing data
+        true_values: list of true labels for testing data
+
+    Testing cycle for HailNet
+
+    """
+    device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+    with torch.no_grad():
+        model.eval()
+        predictions = []
+        true_values = []
+        for xt, yt in test_dl:
+            xt, yt = xt.to(device), yt.to(device)
+            predictions.append(model(xt))
+            true_values.append(yt)
+    return predictions, true_values
